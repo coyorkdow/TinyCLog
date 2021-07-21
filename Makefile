3b20c7d7eee7ea340b50212405703315fcca7302
coyorkdow <coyorkdow@outlook.com>
2021-07-21 21:08:22 +0800

    segment log file by time

--- Makefile
+++ Makefile
@@ -1,9 +1,9 @@
-USER_LOGFILE_DIR = example.log
+USER_LOGFILE_LOCATION = example.log
 USER_LOG_LEVEL = DEBUG
 
 CC = gcc
 CFLAGS = -Wall -O2
-LOG_FLAGS = -DTINY_C_LOG_LOG_IMPL -DUSER_LOGFILE_DIR=\"$(USER_LOGFILE_DIR)\" -DUSER_LOG_LEVEL=DEBUG 
+LOG_FLAGS = -DTINY_C_LOG_LOG_IMPL -DUSER_LOGFILE_DIR=\"$(USER_LOGFILE_LOCATION)\" -DUSER_LOG_LEVEL=DEBUG 
 
 example: example.o log_posix.h.gch
 	$(CC) $(CFLAGS) -o example example.o log_posix.h.gch
@@ -11,7 +11,7 @@ example: example.o log_posix.h.gch
 example.o: example.c
 
 log_posix.h.gch: log_posix.h
-	$(CC) $(LOG_FLAGS) -c log_posix.h
+	$(CC) $(CFLAGS) $(LOG_FLAGS) -c log_posix.h
 
 clean:
 	rm -f *~ *.o *.gch example
--- log_posix.h
+++ log_posix.h
@@ -48,20 +48,29 @@ const enum LogLevel log_level = INFO;
 
 char formatted_time[64];
 
-void InitLogFile() {
+long long file_suffix = 0;
+
+void UpdateFile(int year, int mon, int day, int hour) {
 #ifdef USER_LOGFILE_DIR
-  log_fd =
-      open(USER_LOGFILE_DIR, O_APPEND | O_WRONLY | O_CREAT | O_CLOEXEC, 0644);
+  char buffer[BUFSIZ];
+  long long suffix = year * (long long)1e6 + mon * (int)1e4 + day * 100 + hour;
+  if (file_suffix != suffix) {
+    file_suffix = suffix;
+    buffer[snprintf(buffer, BUFSIZ, USER_LOGFILE_DIR "_%lld", suffix)] = '\0';
+    if (log_fd != -1) close(log_fd);
+    log_fd = open(buffer, O_APPEND | O_WRONLY | O_CREAT | O_CLOEXEC, 0644);
+  }
 #else
-  log_fd = fileno(stderr);
+  if (log_fs == -1) log_fd = fileno(stderr);
 #endif  // USER_LOGFILE_DIR
 }
 
 void DoLog(const char *format, ...) {
   char buffer[BUFSIZ];
-  if (log_fd == -1) InitLogFile();
   time_t t = time(NULL);
   struct tm *fulltime = localtime(&t);
+  UpdateFile(fulltime->tm_year + 1900, fulltime->tm_mon + 1, fulltime->tm_mday,
+             fulltime->tm_hour);
   formatted_time[strftime(formatted_time, sizeof(formatted_time),
                           "%Y-%m-%d %H:%M:%S", fulltime)] = '\0';
   va_list args;
